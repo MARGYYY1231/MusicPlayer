4c4870f992cf83a7aefd38f157928b45ee26bbbd
Margaret Nieves Gonzales <138099922+MARGYYY1231@users.noreply.github.com>
2023-08-28 13:22:40 +1200

    First Part of Music Player
    First Part of Music Player.

---
+++ Music Player/data.js
@@ -0,0 +1,47 @@
+/*
+Boom Clap - Nightcore Wolfie
+Maps - Mirima
+Bring me to Life - Sinnon
+Down - Sinnon
+Alphabet Boy Mashup - Teddie
+Cold Water - Sinnon
+*/
+
+let songs = [
+    {
+        name: 'Bring Me to Life - Nightcore',
+        path: 'music/BringMeToLife.mp3',
+        artist: 'Sinnon',
+        cover: 'images/1.png'
+    },
+    {
+        name: 'Down - Nightcore',
+        path: 'music/Down.mp3',
+        artist: 'Sinnon',
+        cover: 'images/2.png'
+    },
+    {
+        name: 'Cold Water Mashup - Nightcore',
+        path: 'music/Mashup2.mp3',
+        artist: 'Sinnon',
+        cover: 'images/3.png'
+    },
+    {
+        name: 'Alphabet Boy Mashup - Nightcore',
+        path: 'music/Mashup1.mp3',
+        artist: 'Teddie',
+        cover: 'images/4.png'
+    },
+    {
+        name: 'Maps - Nightcore',
+        path: 'music/Maps.mp3',
+        artist: 'Mirima',
+        cover: 'images/5.png'
+    },
+    {
+        name: 'Boom Clap - Nightcore',
+        path: 'music/BoomClap.mp3',
+        artist: 'Nightcore Wolfie',
+        cover: 'images/6.png'
+    },
+]
